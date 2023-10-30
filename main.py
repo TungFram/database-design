f337dfdcde3a154eea10faf8e761b79632931267
Vladimir Sidortsov <TungFram@yandex.ru>
2023-10-30 16:29:05 +0300

    feat: add products.csv

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 import csv
 from random import choice
 
-import random, string
+import random
+import string
 import datetime
-
-filename = 'catalog_nodes.csv'
 
 
 def randomword(length):
@@ -13,41 +12,34 @@ def randomword(length):
     return ''.join(random.choice(letters) for i in range(length))
 
 
+filename = 'products.csv'
 with open(filename, 'w', newline='') as file:
     writer = csv.writer(file)
     field = [
+        "product_id",
         "catalog_node_id",
-        "parent_node_id",
-        "title"
+        "old_price",
+        "current_price",
+        "is_delivery_enabled",
+        "is_pickup_enabled",
+        "title",
+        "description",
     ]
 
     writer.writerow(field)
     for id in range(1, 1000):
-        # Генерируем случайную дату в диапазоне.
-        start_date = datetime.date(2023, 1, 1)
-        end_date = datetime.date(2029, 12, 31)
-        random_date = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days))
-        # преобразуем дату в строку в формате "день-месяц-год"
-        # для строковых значений обязательно f"'{}'"
-        date = f"'{random_date.strftime('%Y-%m-%d')}'"
 
-        # writer.writerow([
-        #     id,  # Id.
-        #     choice([0.1, 0.2, 0.5, 1.0, 2.0, 5.0]),  # Gigabytes.
-        #     choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2.0]),
-        #     choice([0.1, 0.2, 0.5, 1.0, 2.0, 5.0]),   # Minutes.
-        #     choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2.0]),
-        #     choice([0.1, 0.2, 0.5, 1.0, 2.0, 5.0]),   # Sms.
-        #     choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2.0]),
-        #     date,  # created_date.
-        # ])
 
         writer.writerow([
             id,
-            random.randint(1, 999),
+            random.randint(0, 999),
+            choice([350, 400, 450, 500, 550, 600, 650, 700, 800, 1000, 1200, 1500, 2000, 5000, 10000]),
+            choice([350, 400, 450, 500, 550, 600, 650, 700, 800, 1000, 1200, 1500, 2000, 5000, 10000]),
+            choice([True, False]),
+            choice([True, False]),
             randomword(64),
+            randomword(1024),
         ])
-
 
 # import os
 #
