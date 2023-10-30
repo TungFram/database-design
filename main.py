468967209935216ebf42367f01a6cf23be792787
Vladimir Sidortsov <TungFram@yandex.ru>
2023-10-30 16:57:15 +0300

    feat: add product_price.csv

--- main.py
+++ main.py
@@ -11,34 +11,37 @@ def randomword(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
+def randomdate():
+    # Генерируем случайную дату в диапазоне.
+    start_date = datetime.date(2023, 1, 1)
+    end_date = datetime.date(2029, 12, 31)
+    random_date = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days))
+    # преобразуем дату в строку в формате "день-месяц-год"
+    # для строковых значений обязательно f"'{}'"
+    date = f"'{random_date.strftime('%Y-%m-%d')}'"
+    return date
 
-filename = 'products.csv'
+filename = 'product_price.csv'
 with open(filename, 'w', newline='') as file:
     writer = csv.writer(file)
     field = [
-        "product_id",
-        "catalog_node_id",
+        "price_id",
+        "prodict_id",
         "old_price",
         "current_price",
-        "is_delivery_enabled",
-        "is_pickup_enabled",
-        "title",
-        "description",
+        "date",
+        "version",
     ]
 
     writer.writerow(field)
     for id in range(1, 1000):
-
-
         writer.writerow([
             id,
-            random.randint(0, 999),
+            random.randint(1, 999),
             choice([350, 400, 450, 500, 550, 600, 650, 700, 800, 1000, 1200, 1500, 2000, 5000, 10000]),
             choice([350, 400, 450, 500, 550, 600, 650, 700, 800, 1000, 1200, 1500, 2000, 5000, 10000]),
-            choice([True, False]),
-            choice([True, False]),
-            randomword(64),
-            randomword(1024),
+            randomdate(),
+            1,
         ])
 
 # import os
